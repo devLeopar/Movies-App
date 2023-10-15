87c9af4e60116d7176aa713e236ddaa1a24b1d0a
Oktar-Mac <oktarkara@gmail.com>
2023-10-15 19:31:38 +0300

    chore: shorten the name of custom component type

--- src/globalTypes.d.ts
+++ src/globalTypes.d.ts
@@ -1,4 +1,4 @@
 import { FC, PropsWithChildren } from "react";
 
 // Custom Type for a React functional component with props AND CHILDREN
-export type ReactCustomFunctionalComponent<P = {}> = FC<PropsWithChildren<P>>;
+export type RCFC<P = {}> = FC<PropsWithChildren<P>>;

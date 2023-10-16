4f48beb388d62a1c0b51dcb879a90b4d43079119
Oktar-Mac <oktarkara@gmail.com>
2023-10-16 16:46:46 +0300

    feat: enchance api to retrieve multiple movies

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -4,17 +4,17 @@ import { Movie } from "../types/Movie";
 const API_ENDPOINT = "http://www.omdbapi.com/";
 const API_KEY = "8ea39b15";
 
-export const searchMovieByTitle = async (title: string): Promise<Movie> => {
-  const response = await axios.get<Movie>(API_ENDPOINT, {
+export const searchMoviesByTitle = async (title: string): Promise<Movie[]> => {
+  const response = await axios.get(API_ENDPOINT, {
     params: {
       apikey: API_KEY,
-      t: title,
+      s: title,
     },
   });
 
   if (response.data.Response === "False") {
-    throw new Error("Movie not found");
+    throw new Error("Movies not found");
   }
 
-  return response.data;
+  return response.data.Search;
 };

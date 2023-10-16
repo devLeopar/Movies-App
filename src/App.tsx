cf3994fb7e54c7931cb7077118b73a128faa8078
Oktar-Mac <oktarkara@gmail.com>
2023-10-16 16:45:56 +0300

    fix: remove root and make it sourced from app again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MovieProvider } from "./context/MovieContext";
+import { RCFC } from "./globalTypes";
+import SearchMovies from "./components/SearhMovies";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const queryClient = new QueryClient();
+
+export const App: RCFC = ({}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MovieProvider>
+        <SearchMovies />
+      </MovieProvider>
+    </QueryClientProvider>
+  );
+};
